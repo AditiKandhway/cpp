79f15a7e4f92db17769d455d05c1c4245cb41db8
AditiKandhway <aditikandhway1234@gmail.com>
2020-06-24 11:34:15 +0530

    initial commit

---
+++ Array/Max Value In Array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int maximum(int a[],int n)
+{
+	int largest=INT_MIN;
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]>largest)
+		{
+			largest=a[i];
+		}
+	}
+	return largest;
+}
+int main() {
+	int n;
+	cin>>n;
+	int a[1000];
+	for(int i=0;i<n;i++)
+	{
+     cin>>a[i];
+	}
+	cout<<maximum(a,n)<<endl;
+	return 0;
+}
